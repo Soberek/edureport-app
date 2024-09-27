eca4db1601116a6b7d889b1b9064ee09c55f197f
Sober <palpuchowski.k@gmail.com>
2024-09-27 18:03:02 +0200

    feat: project init with configs

---
+++ src/app/ui/nav-links.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavLinks({}) {
+  const pathname = usePathname();
+  return (
+    <nav>
+      <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
+        Home
+      </Link>
+
+      <Link className={`link ${pathname === "/about" ? "active" : ""}`} href="/about">
+        About
+      </Link>
+    </nav>
+  );
+}
