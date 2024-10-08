d797bc33e4037fb85f9be652a2d83d8ccef3c3ba
Sober <palpuchowski.k@gmail.com>
2024-10-08 20:54:16 +0200

    feat: generating miernik and ability to copy to excel

--- src/app/components/ExcelUploader.tsx
+++ src/app/components/ExcelUploader.tsx
@@ -1,15 +1,21 @@
 import React, { useState, ChangeEvent } from "react";
 import * as XLSX from "XLSX";
-import moment from "moment";
+// import moment from "moment";
 
 interface ExcelRow {
   [key: string]: string | number;
 }
 
 const ExcelUploader: React.FC = () => {
-  const [data, setData] = useState<ExcelRow[]>([]);
   const [monthInput, setMonthInput] = useState<number | null>(null);
-  const [months, setMonths] = useState<number[]>([]);
+  const [, setMonths] = useState<number[]>([]);
+  const [agregated_data, setAgregatedData] = useState<{
+    [key: string]: {
+      [key: string]: {
+        [key: string]: { people: number; action_number: number };
+      };
+    };
+  }>({});
 
   const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const blob_xlsx_file = e.target.files?.[0];
@@ -34,10 +40,8 @@ const ExcelUploader: React.FC = () => {
         const ws = wb.Sheets[wsname];
         const data = XLSX.utils.sheet_to_json(ws, { raw: false }) as ExcelRow[];
 
-        agregateData(data);
-
         console.log(data);
-        setData(data);
+        agregateData(data);
       }
     };
 
@@ -53,7 +57,6 @@ const ExcelUploader: React.FC = () => {
         if (find) return prevMonths;
         return [...prevMonths, monthInput];
       });
-    console.log(months);
   }
 
   function handleMonthsInput(event: React.ChangeEvent<HTMLInputElement>) {
@@ -72,41 +75,65 @@ const ExcelUploader: React.FC = () => {
       ["Liczba działań"]: number;
     }[]
   ) {
-    const agregated_data: {
+    const programs_data: {
       [key: string]: {
-        [key: string]: { people: number; action_number: number };
+        [key: string]: {
+          [key: string]: { people: number; action_number: number };
+        };
       };
     } = {};
 
     data.forEach((row) => {
       const program_type = row["Typ programu"];
       const program_name = row["Nazwa programu"];
+      const program_action = row["Działanie"];
       const people_count = Number(row["Liczba ludzi"]);
       const action_count = Number(row["Liczba działań"]);
 
-      const date = moment(row["Data"], "YYYY-MM-DD"); // Assume date format is YYYY-MM-DD
-      const month = date.month() + 1; // moment months are 0-indexed
+      // const date = moment(row["Data"], "YYYY-MM-DD"); // Assume date format is YYYY-MM-DD
+      // const month = date.month() + 1; // moment months are 0-indexed
 
-      console.log(date);
+      console.log(program_action);
+
+      /*
+      {
+      PROGRAMOWE:
+        TF:
+          Koordynacja
+      }
+      */
 
       //   tworzy typy programów
-      if (!agregated_data[program_type]) {
-        agregated_data[program_type] = {};
+      if (!programs_data[program_type]) {
+        programs_data[program_type] = {};
       }
       //   tworzy nazwy programów
-      if (!agregated_data[program_type][program_name]) {
-        agregated_data[program_type][program_name] = {
+      if (!programs_data[program_type][program_name]) {
+        programs_data[program_type][program_name] = {};
+      }
+
+      console.log(programs_data);
+
+      // tworzy nazwy typów akcji
+      if (!programs_data[program_type][program_name][program_action]) {
+        programs_data[program_type][program_name][program_action] = {
           people: 0,
           action_number: 0
         };
       }
 
-      if (!isNaN(people_count) && !isNaN(action_count))
-        agregated_data[program_type][program_name].people += people_count;
-      agregated_data[program_type][program_name].action_number += action_count;
+      if (!isNaN(people_count) && !isNaN(action_count)) {
+        programs_data[program_type][program_name][program_action].people +=
+          people_count;
+        programs_data[program_type][program_name][
+          program_action
+        ].action_number += action_count;
+      }
     });
 
-    console.log(agregated_data);
+    console.log(programs_data);
+    // return;
+    setAgregatedData(programs_data);
   }
 
   return (
@@ -134,26 +161,56 @@ const ExcelUploader: React.FC = () => {
         </form>
         <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
       </div>
-      {data.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map((key) => (
-                <th key={key}>{key}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                {Object.values(row).map((value, i) => (
-                  <td key={i}>{value.toString()}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+
+      <pre>
+        {agregated_data &&
+          Object.keys(agregated_data).map(
+            (program_type, program_type_index) => (
+              <div key={program_type_index} className="mb-4">
+                <h1 className="border-b-2 mb-3 text-lg">
+                  {++program_type_index}. {program_type}
+                </h1>
+
+                <div>
+                  {Object.keys(agregated_data[program_type]).map(
+                    (program_name, program_name_index) => (
+                      <div key={program_name} className="mb-2">
+                        <h1 className="font-bold">{`${++program_name_index}.\t${program_name}\n`}</h1>
+                        {Object.keys(
+                          agregated_data[program_type][program_name]
+                        ).map((program_action, program_action_idx) => (
+                          <div key={program_action_idx} className="">
+                            <>
+                              <span>
+                                {program_name_index}.{++program_action_idx}.
+                                {"\t"}
+                              </span>
+                              <span>{program_action}</span>
+
+                              {"\t"}
+                            </>
+                            {Object.values(
+                              agregated_data[program_type][program_name][
+                                program_action
+                              ]
+                            ).map((counter) => (
+                              <>
+                                <span className="ml-4">
+                                  {`\t`}
+                                  {counter}
+                                </span>
+                              </>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+            )
+          )}
+      </pre>
     </div>
   );
 };
