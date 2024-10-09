55f39ec75235f123cc98ff9e3a8505b3a0a9ea2c
Sober <palpuchowski.k@gmail.com>
2024-10-09 13:40:29 +0200

    feat getting selected months state from child component
    + fix loading data

--- src/app/components/ExcelUploader.tsx
+++ src/app/components/ExcelUploader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import * as XLSX from "xlsx";
 import moment from "moment";
 import { ExcelUploaderMonths } from "./ExcelUploaderMonths";
@@ -6,14 +6,6 @@ import { ExcelUploaderMonths } from "./ExcelUploaderMonths";
 interface ExcelRow {
   [key: string]: string | number;
 }
-
-// interface Data {
-//   [key: string]: {
-//     [key: string]: {
-//       [key: string]: { people: number; action_number: number };
-//     };
-//   };
-// }
 
 interface ProgramsData {
   [key: string]: {
@@ -25,6 +17,8 @@ interface ProgramsData {
 
 const ExcelUploader: React.FC = () => {
   const [agregated_data, setAgregatedData] = useState<ProgramsData>({});
+  const [raw_data, setRawData] = useState<ExcelRow[]>([]);
+  const [selected_months, setSelectedMonths] = useState<number[]>([]);
 
   const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
     const blob_xlsx_file = e.target.files?.[0];
@@ -43,21 +37,35 @@ const ExcelUploader: React.FC = () => {
     reader.onload = (evt: ProgressEvent<FileReader>) => {
       const array_buffer = evt.target?.result;
 
-      if (array_buffer instanceof ArrayBuffer) {
+      if (!array_buffer) {
+        console.error("Failed to load the file. The file data is missing.");
+        return;
+      }
+
+      try {
         const wb = XLSX.read(array_buffer, { type: "array" });
         const wsname = wb.SheetNames[0];
         const ws = wb.Sheets[wsname];
         const data = XLSX.utils.sheet_to_json(ws, { raw: false }) as ExcelRow[];
 
-        agregateData(data);
+        setRawData(data);
+      } catch (error) {
+        console.error("Error reading XLSX file:", error);
       }
     };
 
     reader.readAsArrayBuffer(blob_xlsx_file);
   };
 
+  const getSelectedMonths = (selected_months: number[]) => {
+    setSelectedMonths(selected_months);
+  };
+
   const agregateData = (data: ExcelRow[]) => {
     const programs_data: ProgramsData = {};
+
+    let all_people = 0;
+    let all_actions = 0;
 
     data.forEach((row) => {
       const program_type = row["Typ programu"];
@@ -68,10 +76,15 @@ const ExcelUploader: React.FC = () => {
       const date = moment(row["Data"], "YYYY-MM-DD"); // Assume date format is YYYY-MM-DD
       const month = date.month() + 1; // moment months are 0-indexed
 
-      console.log(month);
+      all_people = all_people + people_count;
+      all_actions = all_actions + action_count;
 
-      //   tworzy typy programów
+      if (selected_months.length > 0 && !selected_months.includes(month)) {
+        return;
+      }
+
       if (!programs_data[program_type]) {
+        //   tworzy typy programów
         programs_data[program_type] = {};
       }
       //   tworzy nazwy programów
@@ -95,20 +108,28 @@ const ExcelUploader: React.FC = () => {
 
     setAgregatedData(programs_data);
   };
+
+  useEffect(() => {
+    if (raw_data.length > 0) {
+      agregateData(raw_data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [raw_data, selected_months]);
+
   return (
     <div className="p-4">
       <h1 className="mb-4 text-xl">Miernik budżetowy</h1>
 
-      <ExcelUploaderMonths />
+      <ExcelUploaderMonths getSelectedMonths={getSelectedMonths} />
 
       <div className="mb-4 border">
         <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
       </div>
 
-      <pre className="mb-4 border">
+      <pre className="mb-4 border p-2">
         {agregated_data &&
           Object.keys(agregated_data).map((program_type, program_type_index) => (
-            <div key={program_type_index} className="mb-4">
+            <div key={program_type_index} className="mb-2">
               <h1 className="mb-3 border-b-2 text-lg">
                 {++program_type_index}. {program_type}
               </h1>
@@ -127,13 +148,11 @@ const ExcelUploader: React.FC = () => {
 
                           {"\t"}
                         </>
-                        {Object.values(agregated_data[program_type][program_name][program_action]).map((counter) => (
-                          <>
-                            <span className="ml-4">
-                              {`\t`}
-                              {counter}
-                            </span>
-                          </>
+                        {Object.values(agregated_data[program_type][program_name][program_action]).map((counter, counter_index) => (
+                          <span key={counter_index} className="ml-4">
+                            {`\t`}
+                            {counter}
+                          </span>
                         ))}
                       </div>
                     ))}
--- src/app/components/ExcelUploaderMonths.tsx
+++ src/app/components/ExcelUploaderMonths.tsx
@@ -7,7 +7,7 @@ interface Month {
 
 type Months = Month[];
 
-export const ExcelUploaderMonths = () => {
+export const ExcelUploaderMonths = ({ getSelectedMonths }: { getSelectedMonths: (selected_months: number[]) => void }) => {
   const [months, setMonths] = useState<Months>([]);
 
   useEffect(() => {
@@ -23,28 +23,34 @@ export const ExcelUploaderMonths = () => {
 
   const handleMonthSelect = (month_to_select: number) => {
     setMonths((prev_months) => {
-      return prev_months.map((month) => (month_to_select === month.month_num ? { ...month, selected: !month.selected } : month));
+      const updated_months = prev_months.map((month) => (month_to_select === month.month_num ? { ...month, selected: !month.selected } : month));
+
+      // Extract selected months after the update
+      const selected_months = updated_months.filter((month) => month.selected).map((month) => month.month_num);
+
+      // Call the parent function with the selected months
+      console.log(selected_months);
+
+      getSelectedMonths(selected_months);
+
+      return updated_months; // Return the updated state
     });
   };
 
-  console.log(months);
-
   return (
-    <>
-      <div className="justify-left mb-4 flex flex-row flex-wrap gap-4">
-        {months.length > 0 &&
-          months.map(({ month_num, selected }, index) => (
-            <button
-              key={index}
-              className={`px-4 py-2 font-bold transition-all hover:-translate-y-1 hover:cursor-pointer ${
-                selected ? "bg-green-500 text-white hover:bg-white hover:text-red-600" : "bg-red-600"
-              } hover:bg-white hover:text-red-600`}
-              onClick={() => handleMonthSelect(month_num)}
-            >
-              {month_num}
-            </button>
-          ))}
-      </div>
-    </>
+    <div className="justify-left mb-4 flex flex-row flex-wrap gap-4">
+      {months.length > 0 &&
+        months.map(({ month_num, selected }, index) => (
+          <button
+            key={index}
+            className={`px-4 py-2 font-bold transition-all hover:-translate-y-1 hover:cursor-pointer ${
+              selected ? "bg-green-500 text-white hover:bg-white hover:text-red-600" : "bg-red-600"
+            } hover:bg-white hover:text-red-600`}
+            onClick={() => handleMonthSelect(month_num)}
+          >
+            {month_num}
+          </button>
+        ))}
+    </div>
   );
 };
