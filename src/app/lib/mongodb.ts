c37b7c8d200ef9cc9016b3e44224324fa0b494be
Sober <59960384+Soberek@users.noreply.github.com>
2024-10-17 20:07:19 +0200

    feat: mongodb connection

---
+++ src/app/lib/mongodb.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
+  return mongoose.connect(process.env.MONGODB_URI as string, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  });
+};
+
+export default connectToDatabase;
