cb7036820cdfcb2581cfe0a9bf3a06e55d692fb2
Sober <59960384+Soberek@users.noreply.github.com>
2024-10-17 20:09:30 +0200

    feat: new main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
+// pages/index.js
+'use client'
+import Head from 'next/head'
+import {  Box, VStack, Heading, Text, Container, Flex } from '@chakra-ui/react'
+import Button from './components/Button'
+
 export default function Home() {
   return (
-    <div className="">
-      <section className="flex flex-row">
-        <div className="flex-1 p-4">
-          <h1>Home</h1>
-          Someday there will be something worth looking at....
-        </div>
-      </section>
-    </div>
-  );
+   
+      <Box minHeight="100vh" display="flex" flexDirection="column">
+        <Head>
+          <title>Generator Sprawozdań OZiPZ</title>
+          <meta name="description" content="Profesjonalny generator sprawozdań online" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Box as="header" bg="ternary.100" color="white" p={4}>
+          <Heading as="h1" size="lg">Generator Sprawozdań</Heading>
+        </Box>
+
+        <Flex as="main" flex="1" alignItems="center" justifyContent="center">
+          <Container maxW="container.md" textAlign="center">
+            <VStack spacing={8}>
+              <Heading as="h2" size="2xl">Twórz profesjonalne sprawozdania w mgnieniu oka</Heading>
+              <Text fontSize="xl">Nasz generator sprawozdań pomoże Ci szybko i łatwo przygotować wysokiej jakości raporty Oświaty Zdrowotnej i Promocji Zdrowia.</Text>
+              <Button label={"Rozpocznij Teraz"} />
+
+            </VStack>
+          </Container>
+        </Flex>
+
+        <Box as="footer" bg="gray.100" p={4} textAlign="center">
+          <Text>&copy; 2024 Generator Sprawozdań. Wszelkie prawa zastrzeżone.</Text>
+        </Box>
+      </Box>
+ 
+  )
 }
-
-// flex flex-col gap-8 row-start-1 sm:items-start
-
-//grid grid-rows-[20px_1fr_20px] justify-items-center gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
-
-// sm:items-start
-// flex flex-col gap-8 row-start-3 sm:items-start
