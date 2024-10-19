f4a5ddcbaaa10a69f36792857c9b07e64bdc33dc
Sober <59960384+Soberek@users.noreply.github.com>
2024-10-19 09:48:45 +0200

    style: animated site background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import { Outlet } from "react-router-dom";
 import SideNavbar from "./components/pages/SideNavbar";
 import { Box, Container } from "@chakra-ui/react";
+import "./index.css";
 
 export default function AppLayout() {
   return (
-    <Container maxWidth="100vw" textColor={`primary.100`} minHeight={`100vh`} bg="fifth.100" margin={0} padding={0}>
+    <Container maxWidth="100vw" textColor={`primary.100`} minHeight={`100vh`} className="site-background" margin={0} padding={0}>
       <Box display="flex">
         <SideNavbar />
         <Box marginLeft={{ base: "0", md: "185px" }} flex={1} overflow={`hidden`}>
