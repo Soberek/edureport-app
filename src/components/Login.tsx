a67f99331b8fa05f32a7a7ec0d0092ba291cf0ac
Sober <palpuchowski.k@gmail.com>
2024-10-21 14:36:35 +0200

    feat: mock login input check with error show

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,15 +2,36 @@ import { Box, FormLabel, Input, Text } from "@chakra-ui/react";
 import Button from "./Button";
 import { Form, Navigate } from "react-router-dom";
 import { AuthContext } from "../services/Auth";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export const Login = () => {
   const auth = useContext(AuthContext);
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
   if (auth.user) return <Navigate to="/miernik-excel" replace />;
 
+  const handleUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setUsername(value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setPassword(value);
+  };
+
   const handleUserLogin = () => {
-    auth.setUser({ user: true });
+    if (username === "12345" && password === "12345") {
+      auth.setUser({ user: true });
+    }
+
+    setError("Nazwa użytkownika lub hasło jest błędne.");
   };
 
   return (
@@ -22,15 +43,21 @@ export const Login = () => {
         <Box display={`flex`} flexDir={`column`} gap={4}>
           <Box>
             <FormLabel htmlFor="username">Nazwa użytkownika</FormLabel>
-            <Input type="text" id="username" />
+            <Input value={username} onChange={handleUsername} type="text" id="username" />
           </Box>
           <Box>
             <FormLabel htmlFor="password">Hasło</FormLabel>
-            <Input type="password" id="password" />
+            <Input value={password} onChange={handlePassword} type="password" id="password" />
           </Box>
         </Box>
       </Form>
       <Button label={`Zaloguj się`} onClick={() => handleUserLogin()} />
+
+      {error && (
+        <Box p={4} bgColor={`red.200`}>
+          {error}
+        </Box>
+      )}
     </Box>
   );
 };
