de6d5b63ef7064cf41eecd8298f167c9f01b8bf2
Sober <palpuchowski.k@gmail.com>
2024-10-23 14:28:28 +0200

    refactor: ExcelUploaderMonths.tsx introduced MaterialUI

--- src/components/molecules/ExcelUploaderMonths.tsx
+++ src/components/molecules/ExcelUploaderMonths.tsx
@@ -1,22 +1,39 @@
-import Button from "../atoms/Button";
-
-import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import Button from "../atoms/Button"; // Adjust the import path as necessary
+import { Box, Typography } from "@mui/material";
 
 export interface Month {
   month_num: number;
   selected: boolean;
 }
 
-export const ExcelUploaderMonths = ({ months, handleMonthSelect }: { months: Month[]; handleMonthSelect: (selected_month: number) => void }) => {
+interface ExcelUploaderMonthsProps {
+  months: Month[];
+  handleMonthSelect: (selected_month: number) => void;
+}
+
+export const ExcelUploaderMonths: React.FC<ExcelUploaderMonthsProps> = ({ months, handleMonthSelect }) => {
   return (
     <>
-      <Text marginBottom={2} fontWeight={700} fontSize={20}>
+      <Typography variant="h6" component="h2" fontWeight={700} marginBottom={2}>
         🦄 Miesiące:
-      </Text>
-      <Box justifyContent={`left`} mb={4} display={`flex`} flexDir={`row`} flexWrap={`wrap`} gap={4}>
+      </Typography>
+      <Box
+        justifyContent="flex-start"
+        marginBottom={4}
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        gap={2} // Adjust gap to your preference
+      >
         {months.length > 0 &&
           months.map(({ month_num, selected }, index) => (
-            <Button key={index} selected={selected} label={month_num} onClick={() => handleMonthSelect(month_num)} />
+            <Button
+              key={index}
+              selected={selected}
+              label={month_num.toString()} // Convert number to string for label
+              onClick={() => handleMonthSelect(month_num)}
+            />
           ))}
       </Box>
     </>
