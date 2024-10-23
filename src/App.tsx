4c67f2b99cbb8330ec354423e46a3d6e329a733d
Sober <59960384+Soberek@users.noreply.github.com>
2024-10-23 20:17:38 +0200

    feat: TopBar component with Logout func

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Topbar } from "./components/layout/Topbar";
 
 export default function AppLayout() {
   return (
     <Box sx={{ margin: 0, padding: 0, color: "primary.main", minHeight: "100vh", paddingLeft: 0 }} className="site-background">
+      <Topbar />
       <Box display="flex">
         <SideNavbar />
-        <Box sx={{ marginLeft: { xs: 0, md: "215px" }, flex: 1, overflow: "hidden" }}>
+        <Box sx={{ marginLeft: { xs: 0, md: "215px" }, marginTop: "50px", flex: 1, overflow: "hidden" }}>
           <Outlet />
         </Box>
       </Box>
---
+++ src/components/layout/Topbar.tsx
@@ -0,0 +1,66 @@
+import { Box, Button, Typography } from "@mui/material";
+import { LuLogOut } from "react-icons/lu";
+import { useContext } from "react";
+import { AuthContext } from "../../context/Auth";
+
+import { Link, useNavigate } from "react-router-dom";
+
+export const Topbar = () => {
+  const navigate = useNavigate();
+  const { setUser } = useContext(AuthContext);
+
+  const handleLogout = async () => {
+    localStorage.removeItem("user");
+    setUser({ user: false });
+    navigate("/login", { replace: true });
+  };
+
+  return (
+    <Box
+      bgcolor={"primary.main"}
+      sx={{ display: "flex", minWidth: "100vw", alignItems: "center", position: "fixed", zIndex: 100, justifyItems: ``, minHeight: "50px", px: 2 }}
+    >
+      <Typography
+        sx={{
+          color: "primary.100",
+          fontSize: 20,
+          ":hover": {
+            fontWeight: "bold"
+          }
+        }}
+      >
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          Generator sprawozdań
+        </Link>
+      </Typography>
+
+      <Button
+        variant="outlined"
+        sx={{
+          ml: "auto",
+          color: "primary.100",
+          backgroundColor: "primary.700",
+
+          ":hover": {
+            transform: `scale(1.1)`
+          }
+        }}
+        onClick={handleLogout}
+      >
+        Wyloguj
+      </Button>
+      <Box
+        sx={{
+          color: "primary.100",
+          mr: 6,
+          ml: 2,
+          ":hover": {
+            transform: `scale(1.1)`
+          }
+        }}
+      >
+        <LuLogOut size={`30`} />
+      </Box>
+    </Box>
+  );
+};
