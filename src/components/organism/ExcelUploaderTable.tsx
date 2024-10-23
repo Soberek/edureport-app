5e97c17e60002caec11c4a923bbc35df29652233
Sober <palpuchowski.k@gmail.com>
2024-10-23 14:29:08 +0200

    refactor: ExcelUploaderTable.tsx introduced MaterialUI

--- src/components/organism/ExcelUploaderTable.tsx
+++ src/components/organism/ExcelUploaderTable.tsx
@@ -1,19 +1,19 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Typography } from "@mui/material";
 
 import { ProgramsData } from "../pages/ExcelUploader";
 
 const TableHeader = () => {
   return (
-    <Box
-      display={"flex"}
-      textAlign={`start`}
-      alignItems={`center`}
-      sx={{ "&>div": { flex: 1, textAlign: "center", fontSize: "1.2rem", fontWeight: "bold" } }}
-      marginBottom={2}
-    >
-      <Box>Działanie</Box>
-      <Box>Liczba działań</Box>
-      <Box>Liczba odbiorców</Box>
+    <Box display={"flex"} textAlign={`start`} alignItems={`center`} marginBottom={2}>
+      <Box flex={1} textAlign="center" fontWeight="bold" fontSize="1.2rem">
+        Działanie
+      </Box>
+      <Box flex={1} textAlign="center" fontWeight="bold" fontSize="1.2rem">
+        Liczba działań
+      </Box>
+      <Box flex={1} textAlign="center" fontWeight="bold" fontSize="1.2rem">
+        Liczba odbiorców
+      </Box>
     </Box>
   );
 };
@@ -23,39 +23,26 @@ const ExcelUploaderTable = (data: ProgramsData) => {
     <>
       {data &&
         Object.entries(data).map(([program_type, program_names], index) => (
-          <Box key={index}>
-            <Text fontSize={"1.3rem"} sx={{ fontWeight: "600" }}>
+          <Box key={index} marginBottom={2}>
+            <Typography variant="h6" fontWeight={600}>
               {program_type}
-            </Text>
+            </Typography>
             <TableHeader />
             {Object.entries(program_names).map(([program_name, action], program_index) => (
-              <Box
-                key={program_index}
-                border={"2px"}
-                borderColor={"gray.400"}
-                display={"flex"}
-                flexDirection={`column`}
-                textAlign={`start`}
-                sx={{
-                  "&>div": {
-                    flex: 1
-                  }
-                }}
-                marginBottom={2}
-              >
-                <Box paddingLeft={1} fontWeight={`bold`}>
+              <Box key={program_index} border={2} borderColor="gray.400" display="flex" flexDirection="column" textAlign="start" marginBottom={2}>
+                <Box paddingLeft={1} fontWeight="bold">
                   {++program_index}. {program_name}
                 </Box>
 
-                <Box display={`flex`} flexDir={`column`} w={"full"}>
+                <Box display="flex" flexDirection="column" width="100%">
                   {Object.entries(action).map(([action_name, action_counters], action_index) => (
-                    <Box key={action_index} display={`flex`} sx={{ "&>div": { flex: 1 } }} paddingLeft={1}>
-                      <Box display="flex">
+                    <Box key={action_index} display="flex" paddingLeft={1}>
+                      <Box display="flex" flex={1}>
                         {program_index}.{++action_index}. {action_name}
                       </Box>
 
                       {Object.entries(action_counters).map(([, counter], counter_index) => (
-                        <Box key={counter_index} textAlign={`center`}>
+                        <Box key={counter_index} flex={1} textAlign="center">
                           {counter}
                         </Box>
                       ))}
