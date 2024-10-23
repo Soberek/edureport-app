d49ff6c9b15546f3416a9eda632a3e63672b244f
Sober <59960384+Soberek@users.noreply.github.com>
2024-10-23 18:21:27 +0200

    style/fix: margin right on main sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // Layout.tsx
 import { Outlet } from "react-router-dom";
 import SideNavbar from "./components/organism/SideNavbar";
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,13 +10,13 @@ import "@fontsource/roboto/700.css";
 
 export default function AppLayout() {
   return (
-    <Container sx={{ margin: 0, maxWidth: `100vw`, color: "primary.main", minHeight: "100vh", padding: 0 }} className="site-background">
+    <Box sx={{ margin: 0, padding: 0, color: "primary.main", minHeight: "100vh", paddingLeft: 0 }} className="site-background">
       <Box display="flex">
         <SideNavbar />
-        <Box sx={{ marginLeft: { xs: 0, md: "185px" }, flex: 1, overflow: "hidden" }}>
+        <Box sx={{ marginLeft: { xs: 0, md: "215px" }, flex: 1, overflow: "hidden" }}>
           <Outlet />
         </Box>
       </Box>
-    </Container>
+    </Box>
   );
 }
