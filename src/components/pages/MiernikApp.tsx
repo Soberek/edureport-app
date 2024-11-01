b3f0932d1c03113f1fe261dff44904816182bb99
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-01 15:33:03 +0100

    refactor: Refactored whole MiernikApp component using Formik and moved logic to its own hook

--- src/components/pages/MiernikApp.tsx
+++ src/components/pages/MiernikApp.tsx
@@ -1,296 +1,147 @@
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import { Box, Button as MUIButton, TextField } from "@mui/material";
+import { Box, Button, MenuItem, TextField } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import moment from "moment";
-import { SelectDropdown } from "../atoms/Select";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { BasicTable } from "../organism/MiernikAppTable";
-
-interface ProgramNameI {
-  _id: string;
-  name: string;
-}
-
-const API_URL: string = import.meta.env.VITE_API_URL;
-
-export interface MiernikItemI {
-  name: string;
-  action_count: number;
-  people_count: number;
-  date: string;
-  owner: string;
-  program_id: { name: string; type: string };
-  action_id: { name: string; id: string };
-}
-
-interface FormDataI {
-  name: string;
-  date: string;
-  program: {
-    name: string;
-    id: string;
-  };
-  action: {
-    name: string;
-    id: string;
-  };
-  people_count: number;
-  action_count: number;
-}
-
-const initial_form_data = {
-  name: "",
-  date: "",
-  program: {
-    name: "",
-    id: ""
-  },
-  action: {
-    name: "",
-    id: ""
-  },
-  people_count: 0,
-  action_count: 0
-};
+import useMiernikAppFormik from "../../hooks/useMiernikApp";
 
 const MiernikApp = () => {
-  const [formData, setFormData] = useState<FormDataI>(initial_form_data);
+  const { handlePostMiernikItem, initial_form_data, validationSchema, miernik_items, actions, program_names, loading } = useMiernikAppFormik();
 
-  const [errors, setErrors] = useState<{
-    name: string;
-    date: string;
-    program_name: string;
-    people_count: string;
-    action_count: string;
-  }>({
-    name: "",
-    date: "",
-    program_name: "",
-    people_count: "",
-    action_count: ""
-  });
-
-  const token = localStorage.getItem("token");
-
-  interface ActionI {
-    _id: string;
-    name: string;
-  }
-
-  const [program_names, setProgramNames] = useState<ProgramNameI[]>([]);
-
-  const [miernik_items, setMiernikItems] = useState<MiernikItemI[] | []>([]);
-
-  const [actions, setActions] = useState<ActionI[] | []>([]);
-
-  const handleSelectChange = useCallback(
-    <T extends { _id: string; name: string }>(e: React.ChangeEvent<HTMLInputElement>, elements: T[], key_to_access_form_data_value: string) => {
-      const id = e.target.value;
-      const position = elements.find((element) => element._id === id);
-
-      if (position) {
-        setFormData((prevFormData) => {
-          return { ...prevFormData, [key_to_access_form_data_value]: { name: position.name, id: position._id } };
-        });
-      }
-    },
-    []
-  );
-
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    setFormData((prevFormData) => {
-      return { ...prevFormData, [name]: value };
-    });
-  }, []);
-
-  const handleDateChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const newDate = moment(new Date(e.target.value)).format("YYYY-MM-DD");
-    setFormData((prevFormData) => {
-      return { ...prevFormData, [e.target.name]: newDate };
-    });
-  }, []);
-
-  const fetchMiernikItems = useCallback(async () => {
-    try {
-      const response = await axios.get(`${API_URL}/api/program_items`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      if (response.status === 200) {
-        const data = response.data;
-
-        setMiernikItems(data);
-      }
-    } catch (err) {
-      console.error(`Error on fetching miernik items`, err);
-    }
-  }, [token]);
-
-  const handlePostMiernikItem = useCallback(async () => {
-    const validateForm = () => {
-      let valid = true;
-      const newErrors = {
-        name: "",
-        date: "",
-        program_type: "",
-        program_name: "",
-        people_count: "",
-        action_count: "",
-        action_id: ""
-      };
-
-      if (!formData.name) {
-        newErrors.name = "Nazwa programu nie może być pusta";
-        valid = false;
-      }
-
-      if (!formData.date) {
-        newErrors.date = "Data nie może być pusta";
-        valid = false;
-      }
-
-      if (!formData.program.name || !formData.program.id) {
-        newErrors.program_name = "Wybierz nazwę programu";
-        valid = false;
-      }
-
-      if (formData.people_count <= 0) {
-        newErrors.people_count = "Liczba osób musi być większa od 0";
-        valid = false;
-      }
-
-      if (formData.action_count <= 0) {
-        newErrors.action_count = "Liczba działań musi być większa od 0";
-        valid = false;
-      }
-
-      if (!formData.action.id || !formData.action.name) {
-        newErrors.action_id = "Wybierz działanie";
-        valid = false;
-      }
-
-      setErrors(newErrors);
-      return valid;
-    };
-
-    const is_valid = validateForm();
-
-    if (!is_valid) {
-      console.log("Formularz zawiera błędy:", errors);
-      return;
-    }
-
-    const new_miernik_item = {
-      ...formData
-    };
-
-    try {
-      const response = await axios.post(
-        `${API_URL}/api/program_item`,
-        {
-          new_miernik_item
-        },
-        {
-          // withCredentials: true
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      );
-
-      console.log(response);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      fetchMiernikItems();
-    }
-  }, [errors, fetchMiernikItems, formData, token]);
-
-  const fetchData = useCallback(async () => {
-    try {
-      const [programNamesResponse, actionsResponse, miernikItemsResponse] = await Promise.allSettled([
-        axios.get(`${API_URL}/api/program_names`, { headers: { Authorization: `Bearer ${token}` } }),
-        axios.get(`${API_URL}/api/program_actions`, { headers: { Authorization: `Bearer ${token}` } }),
-        axios.get(`${API_URL}/api/program_items`, { headers: { Authorization: `Bearer ${token}` } })
-      ]);
-
-      if (programNamesResponse.status === "fulfilled") setProgramNames(programNamesResponse.value.data);
-      if (actionsResponse.status === "fulfilled") setActions(actionsResponse.value.data);
-      if (miernikItemsResponse.status === "fulfilled") setMiernikItems(miernikItemsResponse.value.data);
-    } catch (err) {
-      console.error("Error fetching data:", err);
-    }
-  }, [token]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+  if (loading) return <div style={{ padding: 10 }}>Loading... (best UI experience)</div>;
 
   return (
     <Box p={{ xs: 1, md: 4 }}>
-      <Grid
-        sx={{
-          backgroundColor: "white",
-          p: 2,
-          boxShadow: 2,
-          "&>*>*": {
-            boxSizing: "border-box",
-            p: 1,
-            width: `100%`
-          },
-          rowGap: 1,
-          borderRadius: `5px`
+      <Formik
+        initialValues={initial_form_data}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          handlePostMiernikItem(values);
         }}
-        container
       >
-        <Grid size={{ xs: 12, md: 6 }}>
-          <TextField label="Nazwa" required variant="filled" name="name" value={formData.name} onChange={handleChange} />
-        </Grid>
+        {({ setFieldValue, touched, errors, values }) => (
+          <Form>
+            <Grid
+              container
+              spacing={2}
+              gap={1}
+              p={2}
+              sx={{
+                backgroundColor: "white",
+                boxShadow: 10,
+                borderRadius: `5px`
+              }}
+            >
+              <Grid size={{ xs: 12, sm: 6 }}>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  name="name"
+                  label="Nazwa np. 88/24 - SP3 w Myśliborzu"
+                  error={touched.name && Boolean(errors.name)}
+                  helperText={<ErrorMessage name="name" />}
+                />
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6 }}>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  name="date"
+                  label="Data wykonania zadania"
+                  type="date"
+                  error={touched.date && Boolean(errors.date)}
+                  helperText={<ErrorMessage name="date" />}
+                  slotProps={{
+                    inputLabel: {
+                      shrink: true
+                    }
+                  }}
+                />
+              </Grid>
 
-        <Grid size={{ xs: 12, md: 6 }}>
-          <TextField variant="filled" required type="date" name="date" value={formData.date} onChange={handleDateChange} />
-        </Grid>
+              <Grid size={{ xs: 12, sm: 6 }}>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  name="people_count"
+                  label="People Count"
+                  type="number"
+                  error={touched.people_count && values.people_count < 0}
+                  helperText={<ErrorMessage name="people_count" />}
+                />
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6 }}>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  name="action_count"
+                  label="Action Count"
+                  type="number"
+                  error={touched.action_count && values.action_count < 0}
+                  helperText={<ErrorMessage name="action_count" />}
+                />
+              </Grid>
 
-        <Grid size={{ xs: 12, md: 6 }}>
-          {program_names.length > 0 && (
-            <SelectDropdown
-              label="Nazwa programu"
-              value={formData.program.id}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleSelectChange(event, program_names, "program")}
-              options={program_names}
-            />
-          )}
-        </Grid>
+              {/* Program name */}
+              {program_names.length > 0 && (
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <Field
+                    as={TextField}
+                    select
+                    label="Nazwa programu"
+                    name="program_name"
+                    error={touched.program_name && Boolean(errors.program_name)}
+                    helperText={<ErrorMessage name="program_name" />}
+                    onChange={(e: { target: { value: any } }) => {
+                      const selected_program_name = e.target.value;
+                      const selected_program = program_names.find((program) => program.name === selected_program_name);
+                      setFieldValue("program_name", selected_program_name);
+                      setFieldValue("program_id", selected_program ? selected_program._id : ""); // Set the corresponding ID
+                    }}
+                  >
+                    {program_names.map((program) => (
+                      <MenuItem key={program._id} value={program.name}>
+                        {program.name}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                </Grid>
+              )}
 
-        <Grid size={{ xs: 12, md: 6 }}>
-          {actions.length > 0 && (
-            <SelectDropdown
-              label="Działanie"
-              value={formData.action.id}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleSelectChange(event, actions, "action")}
-              options={actions}
-            />
-          )}
-        </Grid>
+              {actions.length > 0 && (
+                <Grid size={{ xs: 12, sm: 6 }}>
+                  <Field
+                    as={TextField}
+                    select
+                    label="Typ działania"
+                    name="action_name"
+                    error={touched.action_name && Boolean(errors.action_name)}
+                    helperText={<ErrorMessage name="action_name" />}
+                    onChange={(e: { target: { value: any } }) => {
+                      const selected_action_name = e.target.value;
+                      const selected_action = actions.find((action) => action.name === selected_action_name);
+                      setFieldValue("action_name", selected_action_name);
+                      setFieldValue("action_id", selected_action ? selected_action._id : ""); // Set the corresponding ID
+                    }}
+                  >
+                    {actions.map((action) => (
+                      <MenuItem key={action._id} value={action.name}>
+                        {action.name}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                </Grid>
+              )}
 
-        <Grid size={{ xs: 12, md: 6 }}>
-          <TextField label="Liczba działań" required variant="filled" type="number" name="action_count" value={formData.action_count} onChange={handleChange} />
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6 }}>
-          <TextField label="Liczba osób" required variant="filled" type="number" name="people_count" value={formData.people_count} onChange={handleChange} />
-        </Grid>
-
-        <Box sx={{ width: "100%", display: "flex", p: 1, alignItems: "center" }}>
-          <MUIButton sx={{ maxWidth: `150px` }} variant="contained" color="primary" onClick={handlePostMiernikItem}>
-            Dodaj
-          </MUIButton>
-        </Box>
-      </Grid>
+              <Grid size={{ xs: 12, sm: 6 }}>
+                <Button color="primary" variant="contained" type="submit">
+                  Dodaj
+                </Button>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
 
       <Box mt={4}>{miernik_items.length > 0 && <BasicTable data={miernik_items} />}</Box>
     </Box>
---
+++ src/hooks/useMiernikApp.tsx
@@ -0,0 +1,129 @@
+import axios, { AxiosError } from "axios";
+import { useCallback, useEffect, useState } from "react";
+import * as Yup from "yup";
+
+interface FormDataI {
+  name: string;
+  date: string;
+  program_name: string;
+  program_id: string;
+  action_name: string;
+  action_id: string;
+  people_count: number;
+  action_count: number;
+}
+
+const initial_form_data: FormDataI = {
+  name: "",
+  date: "",
+  program_name: "",
+  program_id: "",
+  action_name: "",
+  action_id: "",
+  people_count: 0,
+  action_count: 0
+};
+
+interface ProgramNameI {
+  _id: string;
+  name: string;
+}
+
+export interface MiernikItemI {
+  name: string;
+  action_count: number;
+  people_count: number;
+  date: string;
+  owner: string;
+  program_id: { name: string; type: string };
+  action_id: { name: string; id: string };
+}
+
+interface ActionI {
+  _id: string;
+  name: string;
+}
+
+const API_URL: string = import.meta.env.VITE_API_URL;
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required"),
+  date: Yup.date().required("Required").min(new Date("1900-01-01"), "Przynajmniej 1900-01-01").max(new Date("2050-01-01"), "Max 2050-01-01").nullable(),
+  program_name: Yup.string().required("Program name is required"),
+  program_id: Yup.string().required("Program name is required"),
+  action_name: Yup.string().required("Action id is required"),
+  action_id: Yup.string().required("Action id is required"),
+  people_count: Yup.number().min(0, "0 albo więcej").required("Pole wymagane"),
+  action_count: Yup.number().min(1, "1 albo więcej").required("Pole wymagane")
+});
+
+const useMiernikAppFormik = () => {
+  const token = localStorage.getItem("token");
+
+  const [program_names, setProgramNames] = useState<ProgramNameI[]>([]);
+  const [actions, setActions] = useState<ActionI[]>([]);
+  const [miernik_items, setMiernikItems] = useState<MiernikItemI[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchMiernikItems = useCallback(async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/program_items`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      if (response.status === 200) {
+        setMiernikItems(response.data);
+      }
+    } catch (err) {
+      console.log(err);
+      setError(`Error fetching miernik items: ${err}`);
+    } finally {
+    }
+  }, [token]);
+
+  const handlePostMiernikItem = useCallback(
+    async (values: FormDataI) => {
+      console.log("Posting data...");
+      try {
+        const response = await axios.post(`${API_URL}/api/program_item`, values, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        console.log("Post response:", response.data);
+        fetchMiernikItems();
+      } catch (err) {
+        const error = err as AxiosError;
+
+        console.log(error);
+        setError(`Error posting item: ${error.message}`);
+      }
+    },
+    [fetchMiernikItems, token]
+  );
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const [programNamesResponse, actionsResponse, miernikItemsResponse] = await Promise.allSettled([
+        axios.get(`${API_URL}/api/program_names`, { headers: { Authorization: `Bearer ${token}` } }),
+        axios.get(`${API_URL}/api/program_actions`, { headers: { Authorization: `Bearer ${token}` } }),
+        axios.get(`${API_URL}/api/program_items`, { headers: { Authorization: `Bearer ${token}` } })
+      ]);
+
+      if (programNamesResponse.status === "fulfilled") setProgramNames(programNamesResponse.value.data);
+      if (actionsResponse.status === "fulfilled") setActions(actionsResponse.value.data);
+      if (miernikItemsResponse.status === "fulfilled") setMiernikItems(miernikItemsResponse.value.data);
+    } catch (err) {
+      setError("Error fetching data: " + err);
+    } finally {
+      setLoading(false);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { handlePostMiernikItem, initial_form_data, validationSchema, program_names, actions, miernik_items, loading, error };
+};
+
+export default useMiernikAppFormik;
