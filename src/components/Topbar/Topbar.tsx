2d6967b4c27f6bf2483dd85acf9b96d3702bc03d
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-02 21:25:45 +0100

    feat: topbar avatar

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,19 +1,11 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 // import { LuLogOut } from "react-icons/lu";
-import { useContext } from "react";
-import AuthContext from "../../context/Auth";
 
-import { Link, useNavigate } from "react-router-dom";
+import AccountMenu from "./AccountMenu";
+
+import { Link } from "react-router-dom";
 
 export const Topbar = () => {
-  const navigate = useNavigate();
-  const { logout, username } = useContext(AuthContext);
-
-  const handleLogout = async () => {
-    logout();
-    navigate("/login", { replace: true });
-  };
-
   return (
     <Box
       bgcolor={"primary.main"}
@@ -33,37 +25,7 @@ export const Topbar = () => {
         </Link>
       </Typography>
 
-      <Typography
-        sx={{
-          ml: "auto",
-          mr: 2,
-          color: "primary.100",
-          fontSize: 13,
-          ":hover": {
-            fontWeight: "bold"
-          }
-        }}
-      >
-        Zalogowany jako: {username}
-      </Typography>
-
-      <Button
-        variant="outlined"
-        sx={{
-          color: "primary.100",
-          backgroundColor: "primary.700",
-          mr: 6,
-          // ml: 2,
-          px: 1,
-          fontSize: 12,
-          ":hover": {
-            transform: `scale(1.1)`
-          }
-        }}
-        onClick={handleLogout}
-      >
-        Wyloguj
-      </Button>
+      <AccountMenu />
     </Box>
   );
 };
