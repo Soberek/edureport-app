9784d30c2613c5de120d19a83fcf4818c7dd34d0
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-09 20:28:43 +0100

    feat/refactor/style: topic image generator

--- src/pages/ImageGenerator/ImageGenerator.tsx
+++ src/pages/ImageGenerator/ImageGenerator.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { Box, Typography, Button, AppBar, Toolbar } from "@mui/material";
+import { Box, Typography, Button, AppBar, Toolbar, TextField } from "@mui/material";
 import html2canvas from "html2canvas";
 
 // Predefiniowane kampanie
@@ -9,12 +9,12 @@ const defaultCampaigns = [
     title: "Regularne badania na obecność osteoporozy: Wsparcie dla zdrowych kości"
   },
   {
-    id: 2,
+    id: 48,
     title: "Mammografia"
   },
   {
     id: 3,
-    title: "Energetyki - szkodliwość"
+    title: "Energetyki"
   },
   {
     id: 4,
@@ -31,34 +31,47 @@ type Titles = (typeof defaultCampaigns)[number]["title"];
 interface HealthCampaignTemplateProps {
   title: Titles;
   componentRef: React.Ref<HTMLDivElement>;
+  imagePosition: { topPosition: number; leftPosition: number };
+  id: number;
 }
 
-const HealthCampaignTemplate = ({ title, componentRef }: HealthCampaignTemplateProps) => {
+const HealthCampaignTemplate = ({ id, title, componentRef, imagePosition }: HealthCampaignTemplateProps) => {
   return (
-    <Box ref={componentRef} sx={{ position: "relative", width: 800, height: 600, backgroundColor: "#F26419", padding: 3 }}>
-      {/* Logo w lewym górnym rogu */}
-      <Box
-        sx={{
-          position: "absolute",
-          top: 4,
-          left: 4,
-          width: 80,
-          height: 80,
-          backgroundColor: "white",
-          borderRadius: "50%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <img src="/api/placeholder/80/80" alt="PSSE Logo" style={{ width: 64, height: 64, objectFit: "contain" }} />
-      </Box>
-
-      {/* Tytuł */}
-      <Box sx={{ marginTop: 24, marginLeft: 4 }}>
+    <Box
+      ref={componentRef}
+      sx={{
+        position: "relative",
+        // width: `100%`,
+        maxWidth: "100%",
+        height: "auto",
+        backgroundImage: "url('/src/assets/Listopad.png')",
+        // backgroundRepeat: "no-repeat",
+        backgroundSize: `cover`,
+        aspectRatio: "16/9",
+        overflow: "hidden",
+        padding: 3
+      }}
+    >
+      <Box sx={{ marginTop: 14, marginLeft: 0 }}>
         <Typography variant="h4" sx={{ color: "white", textTransform: "uppercase", maxWidth: 400, lineHeight: "1.25" }}>
           {title}
         </Typography>
+
+        {/* image container */}
+        <Box
+          position={`absolute`}
+          sx={{
+            top: imagePosition.topPosition,
+            left: imagePosition.leftPosition,
+            height: "81%",
+            width: "50%",
+            borderRadius: 3,
+            backgroundColor: `red`,
+            backgroundImage: `url('/src/assets/${id}.png')`,
+            backgroundSize: "cover",
+            transform: "translate(-50%, -50%"
+          }}
+        ></Box>
       </Box>
     </Box>
   );
@@ -69,7 +82,7 @@ const QuickCampaignGenerator = () => {
   const componentRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [exportStatus, setExportStatus] = useState("");
   const [isExporting, setIsExporting] = useState(false);
-
+  const [imagePosition, setImagePosition] = useState({ topPosition: 49, leftPosition: 480 });
   // Eksport pojedynczej kampanii
   const exportSingleCampaign = async (campaign: (typeof defaultCampaigns)[number], index: number) => {
     if (!componentRefs.current[index]) return;
@@ -112,8 +125,26 @@ const QuickCampaignGenerator = () => {
     }, 3000);
   };
 
+  const handleImagePositionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    const name = e.target.name;
+
+    console.log(value, name);
+    if (isNaN(value) || value < 0) {
+      setImagePosition((prevPosition) => {
+        return { ...prevPosition, [name]: 0 };
+      });
+
+      return;
+    }
+    setImagePosition((prevPosition) => {
+      return { ...prevPosition, [name]: value };
+    });
+  };
+
   return (
     <Box sx={{ padding: 3, maxWidth: "1200px", margin: "0 auto" }}>
+      {/* Export button */}
       <AppBar position="sticky" sx={{ backgroundColor: "white", boxShadow: 2 }}>
         <Toolbar sx={{ justifyContent: "space-between" }}>
           <Typography variant="h6" sx={{ fontWeight: "bold" }}>
@@ -130,10 +161,40 @@ const QuickCampaignGenerator = () => {
         </Toolbar>
       </AppBar>
 
+      {/* Image position */}
+      <AppBar position="sticky" sx={{ backgroundColor: "white", boxShadow: 2 }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <TextField
+            type="number"
+            required
+            value={imagePosition.topPosition}
+            label="Top position"
+            name="topPosition"
+            onChange={handleImagePositionChange}
+          />
+          <TextField
+            type="number"
+            required
+            value={imagePosition.leftPosition}
+            label="Left position"
+            name="leftPosition"
+            onChange={handleImagePositionChange}
+          />
+        </Toolbar>
+      </AppBar>
+
       <Box sx={{ marginTop: 4 }}>
         {campaigns.map((campaign, index) => (
-          <Box key={campaign.id} sx={{ borderRadius: "8px", padding: 2, backgroundColor: "white", boxShadow: 1, marginBottom: 3 }}>
-            <HealthCampaignTemplate title={campaign.title} componentRef={(el) => (componentRefs.current[index] = el)} />
+          <Box
+            key={campaign.id}
+            sx={{ borderRadius: "8px", padding: 2, backgroundColor: "white", boxShadow: 1, marginBottom: 3 }}
+          >
+            <HealthCampaignTemplate
+              imagePosition={imagePosition}
+              title={campaign.title}
+              id={campaign.id}
+              componentRef={(el) => (componentRefs.current[index] = el)}
+            />
           </Box>
         ))}
       </Box>
