e3b502e53645715836e9d15caf295bea52b73eed
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-18 16:59:36 +0100

    style: fiexed width for mobile on basicmodel

--- src/components/BasicModal/BasicModal.tsx
+++ src/components/BasicModal/BasicModal.tsx
@@ -7,10 +7,12 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  minWidth: 200,
+  maxWidth: 400,
   bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4
+  boxShadow: 20,
+  p: 2,
+  borderRadius: "5px"
 };
 
 interface BasicModalPropsI {
