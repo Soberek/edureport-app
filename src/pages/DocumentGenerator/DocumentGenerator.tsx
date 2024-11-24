c8e1fff55376eed3d8f789c1f5ad9761be9a1571
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-24 09:30:48 +0000

    Nov 24, 2024, 10:30 AM

--- src/pages/DocumentGenerator/DocumentGenerator.tsx
+++ src/pages/DocumentGenerator/DocumentGenerator.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import PizZip from "pizzip";
 import Docxtemplater from "docxtemplater";
 
-//
+// docx file static on client side
+// every temlate different fields but shared
 
 export const DocumentGenerator = () => {
   return <div>DocumentGenerator</div>;
