e73afb1ccd51782c8c7bdb8d732994ac819a8615
Sober <59960384+Soberek@users.noreply.github.com>
2024-11-24 09:29:11 +0000

    Nov 24, 2024, 10:29 AM

---
+++ src/pages/DocumentGenerator/DocumentGenerator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
+
+export const DocumentGenerator = () => {
+  return <div>DocumentGenerator</div>;
+};
